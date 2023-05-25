ca966572d5f99fc207407bb57de503d5a2d169e1
Matt Radbourne <mail@radbourne.net>
2023-05-25 22:32:28 +0100

    Minor nvim tidying

--- nvim/.config/nvim/lua/mradbourne/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/mradbourne/plugins/neo-tree.lua
@@ -1,5 +1,7 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
 require("neo-tree").setup({
-    close_if_last_window = true,
     filesystem = {
         filtered_items = {
             visible = true,
