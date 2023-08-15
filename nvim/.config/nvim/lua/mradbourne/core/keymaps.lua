b92221f0a322c103f216c01d4bbeeb0ea30ef55c
Matt Radbourne <mail@radbourne.net>
2023-08-15 22:13:02 +0100

    Update keymaps

--- nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
+++ nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
@@ -6,17 +6,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Activity keymaps
-vim.keymap.set('n', '<leader>ae', vim.cmd.Ex)
-vim.keymap.set('n', '-', ':Neotree<cr>')
+vim.keymap.set('n', '-', '<Cmd>Neotree toggle<CR>')
 
 -- Telescope keymaps
 vim.keymap.set('n', '<Tab>', tel.oldfiles)
-vim.keymap.set('n', '<C-p>', tel.git_files)
-vim.keymap.set('n', '<C-f>', tel.live_grep)
+vim.keymap.set('n', '<leader>f', tel.git_files)
+vim.keymap.set('n', '<leader>g', tel.live_grep)
 vim.keymap.set('n', '<leader>b', tel.buffers)
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
