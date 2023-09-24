ec975a21447ffd1d27d44ed12a8cb4a99b9bd137
Matt Radbourne <mail@radbourne.net>
2023-09-24 22:33:07 +0100

    Remove unused neovim keymap

--- nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
+++ nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
@@ -5,9 +5,6 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Activity keymaps
-vim.keymap.set('n', '-', '<Cmd>Neotree toggle<CR>')
-
 -- Telescope keymaps
 vim.keymap.set('n', '<Tab>', tel.oldfiles)
 vim.keymap.set('n', '<leader>f', tel.git_files)
