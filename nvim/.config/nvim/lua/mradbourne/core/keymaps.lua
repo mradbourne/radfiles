6da460229cc534255f7dc02b14d8eb3dc35d237e
Matt Radbourne <mail@radbourne.net>
2023-10-11 22:53:45 +0100

    Add nvim window keymaps

--- nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
+++ nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
@@ -5,6 +5,10 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Window keymaps
+vim.keymap.set('n', '<leader>w', ':bw<CR>')
+vim.keymap.set('n', '<C-m>l', ':Vex<CR><C-w>l')
+
 -- Telescope keymaps
 vim.keymap.set('n', '<Tab>', tel.oldfiles)
 vim.keymap.set('n', '<leader>f', tel.git_files)
