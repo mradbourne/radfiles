1dd415d5ef63697b51b8093cb4137af98442cff9
Matt Radbourne <mail@radbourne.net>
2023-10-14 23:27:23 +0100

    Remove vscode integration for nvim - YAGNI

--- nvim/.config/nvim/lua/mradbourne/init.lua
+++ nvim/.config/nvim/lua/mradbourne/init.lua
@@ -1,28 +1,16 @@
-if (vim.g.vscode) then
-  -- Core
-  require('mradbourne.vscode.keymaps')
-  require('mradbourne.vscode.options')
+-- Plugin manager
+require('mradbourne.packer')
 
-  -- Plugins
-  require('mradbourne.plugins.comment')
+-- Core
+require('mradbourne.core.colorscheme')
+require('mradbourne.core.keymaps')
+require('mradbourne.core.options')
 
-  -- Utils
-  require('mradbourne.utils.highlight-on-yank')
-else
-  -- Plugin manager
-  require('mradbourne.packer')
+-- Plugins
+require('mradbourne.plugins.comment')
+require('mradbourne.plugins.gitsigns')
+require('mradbourne.plugins.mason')
+require('mradbourne.plugins.telescope')
 
-  -- Core
-  require('mradbourne.core.colorscheme')
-  require('mradbourne.core.keymaps')
-  require('mradbourne.core.options')
-
-  -- Plugins
-  require('mradbourne.plugins.comment')
-  require('mradbourne.plugins.gitsigns')
-  require('mradbourne.plugins.mason')
-  require('mradbourne.plugins.telescope')
-
-  -- Utils
-  require('mradbourne.utils.highlight-on-yank')
-end
+-- Utils
+require('mradbourne.utils.highlight-on-yank')
--- nvim/.config/nvim/lua/mradbourne/vscode/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Remap space as leader key
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Telescope keymaps
-vim.keymap.set('n', '<leader>f', function()
-    vim.fn.VSCodeNotify('workbench.action.quickOpen')
-end)
