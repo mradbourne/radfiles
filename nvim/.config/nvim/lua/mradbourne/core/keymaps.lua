64d00ff2111a3b47eec504754ff9a049735ab37f
Matt Radbourne <mail@radbourne.net>
2023-11-30 22:21:32 +0000

    Update nvim keymaps

--- nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
+++ nvim/.config/nvim/lua/mradbourne/core/keymaps.lua
@@ -6,11 +6,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Window keymaps
-vim.keymap.set('n', '<leader>w', ':bw<CR>')
 vim.keymap.set('n', '<C-m>l', ':Vex<CR><C-w>l')
 
 -- Telescope keymaps
-vim.keymap.set('n', '<Tab>', tel.oldfiles)
+vim.keymap.set('n', '<Tab>', tel.buffers)
 vim.keymap.set('n', '<leader>f', tel.git_files)
 vim.keymap.set('n', '<leader>g', tel.live_grep)
-vim.keymap.set('n', '<leader>b', tel.buffers)
