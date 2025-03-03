4a0c563f2447f13b1b16e997182d371ee0658e95
Matt Radbourne <1254508+mradbourne@users.noreply.github.com>
2025-03-03 22:19:39 +0000

    Improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+
+# Set up stow
 if ! command -v stow &> /dev/null; then
     echo "stow not found, installing..."
     if [[ "$OSTYPE" == "darwin"* ]]; then
+        if ! command -v brew &> /dev/null; then
+            echo "brew not found. Please install homebrew first."
+            exit 1
+        fi
         brew install stow
     elif [[ -f /etc/debian_version ]]; then
         sudo apt-get update && sudo apt-get install -y stow
@@ -12,9 +18,14 @@ if ! command -v stow &> /dev/null; then
         exit 1
     fi
 fi
-
 stow stowfiles
 
+# Source .bashrc-extra, which is the entry point for all other dotfiles
 if [ ! -f ~/.bashrc ]; then
     echo "source ~/.bashrc-extra" > ~/.bashrc
+else
+    echo "You already have a .bashrc file. Please add 'source ~/.bashrc-extra' to it."
 fi
+
+echo "Consider installing pyenv"
+echo "Done!"
